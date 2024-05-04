9aae6148c50c30d193c36a5f0e8cc138cfd9620c
newo200 <newo200coding@gmail.com>
2024-05-04 15:31:03 +1000

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Footer() {
+    return (
+    <div className="container footer">
+        <footer class="mt-5">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="./home.html" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="./about.html" class="nav-link px-2 text-body-secondary">About</a></li>
+            <li class="nav-item"><a href="./blog.html" class="nav-link px-2 text-body-secondary">Blog</a></li>                  </ul>
+        <p class="text-center text-body-secondary text-font">© 2024 Owen's Personal Portfolio</p>
+        </footer>
+    </div>
+    )
+}
+
+export default Footer
