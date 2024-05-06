dbd70d049a13ab6353685c196f9d14fb8b6677bb
newo200 <newo200coding@gmail.com>
2024-05-06 18:57:28 +1000

    Button Redirects

--- src/components/blog-card.jsx
+++ src/components/blog-card.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useNavigate } from "react-router-dom"
 
 function BlogCard(props) {
 
+  const navigate = useNavigate()
+
   function test(event) {
     console.log(event.target.id)
+  }
+
+  function editBlog(event) {
+    const id = event.target.id
+    navigate(`/blog/edit/${id}`)
   }
 
   return (
@@ -17,7 +25,7 @@ function BlogCard(props) {
             {props.description}
             </Card.Text>
             <Button onClick = {test} id = {props.id} className = "mx-1" variant="primary">Read Blog</Button>
-            <Button id = {props.id} className = "mx-1" variant="primary">Edit</Button>
+            <Button onClick = {editBlog} id = {props.id} className = "mx-1" variant="primary">Edit</Button>
             <Button id = {props.id} className = "mx-1" variant="primary">Delete</Button>
         </Card.Body>
         </Card>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,14 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 function Hero() {
+
+    const navigate = useNavigate()
+
+    function getBlogs() {
+        navigate("/blog")
+    }
+
     return (
         <div className="px-4 py-5 my-5 text-center">
             <img className="d-block mx-auto mb-4 profile" src={require("../images/Professional Headshot.jpg")} alt="" width="229" height="185"/>
@@ -8,7 +16,7 @@ function Hero() {
             <div className="col-lg-6 mx-auto">
             <p className="lead mb-4 text-font">Economist, Cricketer and Aspiring Programmer</p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Read Blogs</button>
+                <button onClick = {getBlogs} type="button" className="btn btn-primary btn-lg px-4 gap-3">Read Blogs</button>
             </div>
             </div>
         </div> 
